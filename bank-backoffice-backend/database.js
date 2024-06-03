ef24fd7e78271dc08058930e21f817593988e178
fatmaboodai <Fatmaboodai@hotmail.com>
2024-06-03 21:17:03 +0300

    "Added MySQL connection configuration to database.js"

--- bank-backoffice-backend/database.js
+++ bank-backoffice-backend/database.js
@@ -1,1 +1,14 @@
-var mysql = require("mysql2")
+const mysql = require("mysql2")
+
+const connectionDB = mysql.createConnection({
+    // pass host , database , user , password
+host: 'localhost',
+database: 'bank_app',
+user:'root',
+password:'fatma@2001$_'
+})
+
+
+
+// export the connection 
+module.exports = connectionDB;
--- bank-backoffice-backend/index.js
+++ bank-backoffice-backend/index.js
@@ -1,12 +1,31 @@
-var express = require('express');
-var mysql = require('mysql2');
-var app = express();
+const express = require('express');
+const mysql = require('mysql2');
+const app = express();
+const connectionDB =  require('./database')
 
-app.listen(5000, function(){
-    console.log('App listening on port 5000');
-});
+
+
 
 
 app.get('/',function(req,res){
-    res.send('Hello World');
+   
+    connectionDB.query('SELECT * FROM Customers',function(err,result){
+        if(err) throw err;
+        // print the data retrived 
+        res.send(result)
+    });
 })
+
+
+
+
+
+app.listen(5000, function(){
+    console.log('App listening on port 5000');
+    connectionDB.connect(function(err){
+        // if there is an error 
+        if(err) throw err;
+        // else console log the connection status
+        console.log('Connected to the database');
+    })
+});
