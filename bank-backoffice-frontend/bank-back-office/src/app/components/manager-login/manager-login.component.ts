510444ded8a83887b260e983847549f4205e0330
fatmaboodai <Fatmaboodai@hotmail.com>
2024-06-06 23:32:29 +0300

    all components , interface , services created

---
+++ bank-backoffice-frontend/bank-back-office/src/app/components/manager-login/manager-login.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manager-login',
+  standalone: true,
+  imports: [],
+  templateUrl: './manager-login.component.html',
+  styleUrl: './manager-login.component.css'
+})
+export class ManagerLoginComponent {
+
+}
--- bank-backoffice-frontend/my-angular-app/src/app/interfaces/data.ts
+++
@@ -1,6 +0,0 @@
-export interface Data {
-    userid: number;
-    id: number;
-    title: string;
-    body: string;
-}
